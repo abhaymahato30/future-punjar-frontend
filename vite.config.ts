a0856b29f668df16413424fc66f53b0dcec5c76c
abhaymahato30 <vikrammahato098@gmail.com>
2025-04-25 17:58:01 +0530

    edit vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['future-punjar-frontend.onrender.com'], // Add your host here
+  },
 });
